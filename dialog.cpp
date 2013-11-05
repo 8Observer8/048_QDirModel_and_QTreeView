230b1b79a22299dcacedd0605f5e5b915c9620fd
Enzhaev Ivan <8observer8@gmail.com>
2013-11-05 05:47:44 +0300

    first commit

---
+++ dialog.cpp
@@ -0,0 +1,59 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    model = new QDirModel(this);
+    model->setReadOnly(false);
+    model->setSorting(QDir::DirsFirst | QDir::IgnoreCase | QDir::Name);
+
+    ui->treeView->setModel(model);
+
+    QModelIndex index = model->index("D:/");
+
+    ui->treeView->expand(index);
+    ui->treeView->scrollTo(index);
+    ui->treeView->setCurrentIndex(index);
+    ui->treeView->resizeColumnToContents(0);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    // Make dir
+    QModelIndex index = ui->treeView->currentIndex();
+    if (!index.isValid()) {
+        return;
+    }
+
+    QString name = QInputDialog::getText(this, "Name", "Enter a name");
+    if (name.isEmpty()) {
+        return;
+    }
+
+    model->mkdir(index, name);
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    // Delete
+    QModelIndex index = ui->treeView->currentIndex();
+    if (!index.isValid()) {
+        return;
+    }
+
+    // Is a dir or a file?
+    if(model->fileInfo(index).isDir()) {
+        model->rmdir(index);
+    } else {
+        model->remove(index);
+    }
+}
